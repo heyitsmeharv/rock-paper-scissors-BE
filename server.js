c593459e38946067afb7cd157fec0c51295ff922
Adam Harvey <heyitsmeharv@gmail.com>
2022-07-23 10:26:05 +0100

    test

--- server.js
+++ server.js
@@ -7,8 +7,17 @@ const server = createServer(app);
 const socket = new Server(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Access-Control-Allow-Origin"],
+    // methods: ["GET", "POST"],
+    // allowedHeaders: ["Access-Control-Allow-Origin"],
+  },
+  handlePreflightRequest: (req, res) => {
+    res.writeHead(200, {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,POST",
+      "Access-Control-Allow-Headers": "my-custom-header",
+      "Access-Control-Allow-Credentials": true,
+    });
+    res.end();
   }
 });
 
